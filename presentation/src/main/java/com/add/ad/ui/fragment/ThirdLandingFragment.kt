6ba21ce37839912a5dc289eebad571fbc65920af
윤석준 <tjrwns8024@naver.com>
2020-09-14 16:08:59 +0900

    [CREATE] ThirdLandingPage 구현

---
+++ presentation/src/main/java/com/add/ad/ui/fragment/ThirdLandingFragment.kt
@@ -0,0 +1,20 @@
+package com.add.ad.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.add.ad.R
+
+class ThirdLandingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third_landing, container, false)
+    }
+
+}
