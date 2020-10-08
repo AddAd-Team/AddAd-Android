e7d96e0513659bf1d6ec07e711ed06a80e58e3d7
윤석준 <tjrwns8024@naver.com>
2020-10-08 16:38:44 +0900

    [UPDATE] KOTLIN -> JAVA
    프로젝트 대이동

---
+++ app/src/main/java/com/add/ad/presentation/ui/fragment/landing/ThirdLandingFragment.java
@@ -0,0 +1,21 @@
+package com.add.ad.presentation.ui.fragment.landing;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.add.ad.R;
+
+public class ThirdLandingFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third_landing, container, false);
+    }
+}
--- domain/src/main/java/com/add/domain/base/Message.kt
+++
@@ -1,2 +0,0 @@
-package com.add.domain.base
-
