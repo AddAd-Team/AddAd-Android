31e83f3f452a6d7fac6fcab1102db201e8456be1
윤석준 <tjrwns8024@naver.com>
2020-10-10 16:01:35 +0900

    [ADD] <MyPageFragment> mypage fragment 추가

---
+++ app/src/main/java/com/add/ad/presentation/ui/fragment/main/mypage/MyPageFragment.java
@@ -0,0 +1,21 @@
+package com.add.ad.presentation.ui.fragment.main.mypage;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.add.ad.R;
+
+public class MyPageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_page, container, false);
+    }
+}
