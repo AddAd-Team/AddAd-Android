97290950e5d410df6ca03b9decc0d8a1048fd2e2
윤석준 <tjrwns8024@naver.com>
2020-10-11 19:38:08 +0900

    [MODIFY] <MainFragment.java> bottomNavigation과 aac Navigation 연결

--- app/src/main/java/com/add/ad/presentation/ui/fragment/MainFragment.java
+++ app/src/main/java/com/add/ad/presentation/ui/fragment/MainFragment.java
@@ -1,18 +1,14 @@
 package com.add.ad.presentation.ui.fragment;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.add.ad.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -25,63 +21,11 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_main, container, false);
+        NavController navController = Navigation.findNavController(v.findViewById(R.id.main_container));
+
         main_navigation = (BottomNavigationView) v.findViewById(R.id.main_navigation);
+        NavigationUI.setupWithNavController(main_navigation,navController);
 
         return v;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        main_navigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
-    }
-
-    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            switch (menuItem.getItemId()) {
-                case R.id.feed_fragment:
-                    return selectFeed();
-                case R.id.search_fragment:
-                    return selectSearch();
-                case R.id.write_fragment:
-                    return selectWrite();
-                case R.id.contact_fragment:
-                    return selectContact();
-                case R.id.mypage_fragment:
-                    return selectMyPage();
-            }
-            return false;
-        }
-    };
-
-    private void replaceFragment(int action) {
-        
-    }
-
-    private Boolean selectFeed() {
-        replaceFragment(R.id.action_global_FeedFragment);
-        return true;
-    }
-
-    private Boolean selectSearch() {
-        replaceFragment(R.id.action_global_SearchFragment);
-        return true;
-    }
-
-    private Boolean selectWrite() {
-        replaceFragment(R.id.action_global_WriteFragment);
-        return true;
-    }
-
-    private Boolean selectContact() {
-        replaceFragment(R.id.action_global_ContactFragment);
-        return true;
-    }
-
-    private Boolean selectMyPage() {
-        replaceFragment(R.id.action_global_MyPageFragment);
-        return true;
     }
 }
