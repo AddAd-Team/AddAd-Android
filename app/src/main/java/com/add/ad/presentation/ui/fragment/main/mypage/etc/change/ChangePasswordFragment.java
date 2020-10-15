0427a0acbdb589c7a68addcf4f061e17a2d5717a
윤석준 <tjrwns8024@naver.com>
2020-10-15 19:11:39 +0900

    [ADD] <ChangePasswordFragment.java> 비밀번호 변경 fragment 추가

---
+++ app/src/main/java/com/add/ad/presentation/ui/fragment/main/mypage/etc/change/ChangePasswordFragment.java
@@ -0,0 +1,29 @@
+package com.add.ad.presentation.ui.fragment.main.mypage.etc.change;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.add.ad.R;
+
+public class ChangePasswordFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_change_password, container, false);
+    }
+
+}
