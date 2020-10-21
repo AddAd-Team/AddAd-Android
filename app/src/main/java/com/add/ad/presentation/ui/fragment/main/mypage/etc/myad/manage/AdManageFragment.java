3c8463cf4b5ea9d14b4f95332e31f5545ff7c6cb
윤석준 <tjrwns8024@naver.com>
2020-10-22 08:27:48 +0900

    [ADD] <AdManageFragment> ui, navigate 추가

---
+++ app/src/main/java/com/add/ad/presentation/ui/fragment/main/mypage/etc/myad/manage/AdManageFragment.java
@@ -0,0 +1,21 @@
+package com.add.ad.presentation.ui.fragment.main.mypage.etc.myad.manage;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.add.ad.R;
+
+public class AdManageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ad_manage, container, false);
+    }
+}
