fa97b46b87271e15c1ef1c1cbc4cd0db26c78e44
윤석준 <tjrwns8024@naver.com>
2020-11-01 22:53:13 +0900

    [ADD] <WriteFragment *> First, Second Write Fragment 추가

--- app/src/main/java/com/add/ad/presentation/ui/fragment/main/write/SecondWriteFragment.java
+++ app/src/main/java/com/add/ad/presentation/ui/fragment/main/write/SecondWriteFragment.java
@@ -10,12 +10,12 @@ import android.view.ViewGroup;
 
 import com.add.ad.R;
 
-public class WriteFragment extends Fragment {
+public class SecondWriteFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_write, container, false);
+        return inflater.inflate(R.layout.fragment_second_write, container, false);
     }
 }
