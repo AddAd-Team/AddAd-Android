c7d2b78d5b39aaacd8431c9fefbefcb7a43f8fe1
윤석준 <tjrwns8024@naver.com>
2020-11-05 22:41:43 +0900

    [MODIFY] <Api.java> @Body -> @Query("password")

--- app/src/main/java/com/add/ad/data/api/Api.java
+++ app/src/main/java/com/add/ad/data/api/Api.java
@@ -2,6 +2,7 @@ package com.add.ad.data.api;
 
 import com.add.ad.entity.Auth;
 import com.add.ad.entity.Post;
+import com.add.ad.entity.ResponseUserInfo;
 import com.add.ad.entity.Token;
 import com.add.ad.entity.User;
 
@@ -11,10 +12,12 @@ import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Response;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Part;
+import retrofit2.http.Query;
 
 public interface Api {
     @POST("/api/auth/signin")
@@ -40,5 +43,8 @@ public interface Api {
                                      @Part("deadline") RequestBody adEndDate);
 
     @PUT("/api/mypage/passwordChange")
-    Single<Response<Void>> changePassword(@Body String password);
+    Single<Response<Void>> changePassword(@Query("password") String password);
+
+    @GET("/api/mypage/profile")
+    Single<Response<ResponseUserInfo>> getUserProfile();
 }
