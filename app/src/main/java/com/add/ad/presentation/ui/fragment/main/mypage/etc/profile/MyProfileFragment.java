1b3726244e1308d7297017ef2b5e8120265d276b
윤석준 <tjrwns8024@naver.com>
2020-11-05 22:43:03 +0900

    .

--- app/src/main/java/com/add/ad/presentation/ui/fragment/main/mypage/etc/profile/MyProfileFragment.java
+++ app/src/main/java/com/add/ad/presentation/ui/fragment/main/mypage/etc/profile/MyProfileFragment.java
@@ -2,22 +2,32 @@ package com.add.ad.presentation.ui.fragment.main.mypage.etc.profile;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.add.ad.R;
+import com.add.ad.databinding.FragmentMyPageBinding;
+import com.add.ad.presentation.base.BaseFragment;
+import com.add.ad.presentation.viewModel.mypage.MyPageViewModel;
+import com.add.ad.presentation.viewModel.write.WriteViewModel;
 
 public class MyProfileFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View v =  inflater.inflate(R.layout.fragment_my_profile, container, false);
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_my_profile, container, false);
         return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 }
