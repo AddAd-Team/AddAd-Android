fe32b356a02233ca9693c2c2b443b9cfe9efe6c2
윤석준 <tjrwns8024@naver.com>
2020-11-06 08:55:01 +0900

    [MODIFY] <FeedFragment.java>

--- app/src/main/java/com/add/ad/presentation/ui/fragment/main/feed/FeedFragment.java
+++ app/src/main/java/com/add/ad/presentation/ui/fragment/main/feed/FeedFragment.java
@@ -9,13 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.add.ad.R;
+import com.add.ad.presentation.viewModel.feed.FeedViewModel;
 
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class FeedFragment extends Fragment {
+    private FeedViewModel feedViewModel;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_feed, container, false);
     }
 }
