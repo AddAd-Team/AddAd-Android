03b69ebfa381a3909277276b93157f87e286f077
윤석준 <tjrwns8024@naver.com>
2020-11-06 16:53:27 +0900

    [MODIFY] <WriteRepository.java> Post 삭제 -> import문 삭제

--- app/src/main/java/com/add/ad/data/repository/write/WriteRepository.java
+++ app/src/main/java/com/add/ad/data/repository/write/WriteRepository.java
@@ -1,12 +1,9 @@
 package com.add.ad.data.repository.write;
 
-import com.add.ad.entity.Post;
-
 import io.reactivex.Single;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Response;
-import retrofit2.http.Part;
 
 public interface WriteRepository {
     Single<Response<Void>> postWrite(MultipartBody.Part file, RequestBody postTitle, RequestBody postTag, RequestBody postContent, RequestBody postPrice, RequestBody postEndDate, RequestBody adEndDate);
