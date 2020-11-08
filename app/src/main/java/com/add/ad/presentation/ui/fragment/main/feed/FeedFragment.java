420dfa2dcff6af344a9a364683f71db515206722
윤석준 <tjrwns8024@naver.com>
2020-11-08 21:22:37 +0900

    [MODIFY] <FeedFragment.java> adapter 세팅

--- app/src/main/java/com/add/ad/presentation/ui/fragment/main/feed/FeedFragment.java
+++ app/src/main/java/com/add/ad/presentation/ui/fragment/main/feed/FeedFragment.java
@@ -1,24 +1,53 @@
 package com.add.ad.presentation.ui.fragment.main.feed;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 import com.add.ad.R;
+import com.add.ad.databinding.FragmentFeedBinding;
+import com.add.ad.entity.response.ResponseFeedInfo;
+// import com.add.ad.presentation.adapter.FeedAdapter;
+import com.add.ad.presentation.adapter.FeedAdapter;
+import com.add.ad.presentation.base.BaseFragment;
 import com.add.ad.presentation.viewModel.feed.FeedViewModel;
+
+import java.util.ArrayList;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
-public class FeedFragment extends Fragment {
+public class FeedFragment extends BaseFragment<FragmentFeedBinding> {
     private FeedViewModel feedViewModel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        setLayout(R.layout.fragment_feed);
+        View v = super.onCreateView(inflater, container, savedInstanceState);
+        feedViewModel = new ViewModelProvider(requireActivity()).get(FeedViewModel.class);
+
+        binding.setVm(feedViewModel);
+        feedViewModel.getFeed();
+
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        feedViewModel.feedListEvent.observe(this, mVoid -> {
+            binding.feedRecyclerView.setAdapter(new FeedAdapter(feedViewModel.feedList.getValue()));
+            binding.feedRecyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        });
+
     }
 }
