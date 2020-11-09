42a61e0c816cdcbbd014013925a1c6e1cffa17fb
윤석준 <tjrwns8024@naver.com>
2020-11-09 21:39:56 +0900

    [ADD] <presentation/FeedAdapter.java> Add feed adapter

--- app/src/main/java/com/add/ad/presentation/adapter/FeedAdapter.java
+++ app/src/main/java/com/add/ad/presentation/adapter/FeedAdapter.java
@@ -2,40 +2,43 @@ package com.add.ad.presentation.adapter;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.MutableLiveData;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.add.ad.R;
 import com.add.ad.databinding.ItemFeedBinding;
 import com.add.ad.entity.response.ResponseFeedInfo;
+import com.add.ad.presentation.viewModel.feed.FeedViewModel;
 
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class FeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private ArrayList<ResponseFeedInfo> feedItems;
+    FeedViewModel feedViewModel;
 
-    public FeedAdapter(ArrayList<ResponseFeedInfo> feedItems) {
+    public FeedAdapter(ArrayList<ResponseFeedInfo> feedItems, FeedViewModel feedViewModel) {
         this.feedItems = feedItems;
+        this.feedViewModel = feedViewModel;
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        Log.d("onCreateviewHolder", String.valueOf(viewType));
         ItemFeedBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.item_feed ,parent,false);
         return new FeedViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        Log.d("onBindViewHolder", holder.toString());
         if(holder instanceof FeedViewHolder) {
-            ((FeedViewHolder) holder).bind(feedItems.get(position));
+            ((FeedViewHolder) holder).bind(feedItems.get(position), position);
         }
     }
 
@@ -44,25 +47,27 @@ public class FeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         return Objects.requireNonNull(feedItems).size();
     }
 
-    public void setFeedItems(MutableLiveData<ArrayList<ResponseFeedInfo>> feedItem){
-        Log.d("setFeedItems","sdfs");
-        this.feedItems = feedItem.getValue();
-        notifyDataSetChanged();
-    }
-
     public class FeedViewHolder extends RecyclerView.ViewHolder {
         ItemFeedBinding binding;
         ResponseFeedInfo item;
+        int position;
+
         FeedViewHolder(ItemFeedBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
-            Log.d("viewholder", String.valueOf(binding));
         }
 
-        void bind(ResponseFeedInfo item) {
+        void bind(ResponseFeedInfo item, int position) {
             this.item = item;
+            this.position = position;
+            binding.setVh(this);
             binding.setFeedItem(item);
+            binding.executePendingBindings();
         }
 
+        public void clickImage(View v){
+            feedViewModel.feedDetailEvent.call();
+            feedViewModel.getDetailFeed(position);
+        }
     }
 }
