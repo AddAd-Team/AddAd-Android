05bf95300fe5bfc42cb99a75cbb629145e77dc20
윤석준 <tjrwns8024@naver.com>
2020-11-10 23:32:56 +0900

    [MODIFY] <fragment/ SearchFragment.java> add bindingFragment & add observing func

--- app/src/main/java/com/add/ad/presentation/ui/fragment/main/search/SearchFragment.java
+++ app/src/main/java/com/add/ad/presentation/ui/fragment/main/search/SearchFragment.java
@@ -1,21 +1,47 @@
 package com.add.ad.presentation.ui.fragment.main.search;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.add.ad.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
-public class SearchFragment extends Fragment {
+import com.add.ad.R;
+import com.add.ad.databinding.FragmentSearchBinding;
+import com.add.ad.presentation.adapter.SearchAdapter;
+import com.add.ad.presentation.base.BaseFragment;
+import com.add.ad.presentation.viewModel.search.SearchViewModel;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class SearchFragment extends BaseFragment<FragmentSearchBinding> {
+    private SearchViewModel searchViewModel;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setLayout(R.layout.fragment_search);
+
+        View v = super.onCreateView(inflater, container, savedInstanceState);
+        searchViewModel = new ViewModelProvider(requireActivity()).get(SearchViewModel.class);
+
+        binding.setVm(searchViewModel);
+        searchViewModel.getCreatorList();
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        searchViewModel.searchListEvent.observe(this, mVoid -> {
+            binding.searchCreatorRecyclerView.setAdapter(new SearchAdapter(searchViewModel.searchList.getValue(),searchViewModel));
+            binding.searchCreatorRecyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        });
     }
 }
