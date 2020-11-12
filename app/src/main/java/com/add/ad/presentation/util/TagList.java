cb3fd5ca2ce15ea871b4765d6052ffba27edbb97
윤석준 <tjrwns8024@naver.com>
2020-11-12 22:19:14 +0900

    [CREATE] <util/ TagList.java> add Tag List

---
+++ app/src/main/java/com/add/ad/presentation/util/TagList.java
@@ -0,0 +1,16 @@
+package com.add.ad.presentation.util;
+
+import java.util.ArrayList;
+
+public class TagList {
+    public static ArrayList<String> getTagList(){
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("유튜브");
+        arrayList.add("블로그");
+        arrayList.add("SNS");
+        arrayList.add("전자기기");
+        arrayList.add("옷");
+        arrayList.add("+");
+        return arrayList;
+    }
+}
