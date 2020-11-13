4217b6d29a5577b13c2045ad32c8a8a616371b83
윤석준 <tjrwns8024@naver.com>
2020-11-13 14:56:51 +0900

    [ADD] <viewmodel/ SearchViewModel.java> add searchTag 7 searchDetailCreator function

--- app/src/main/java/com/add/ad/presentation/viewModel/search/SearchViewModel.java
+++ app/src/main/java/com/add/ad/presentation/viewModel/search/SearchViewModel.java
@@ -9,6 +9,7 @@ import com.add.ad.data.repository.search.SearchRepository;
 import com.add.ad.entity.response.ResponseSearchInfo;
 import com.add.ad.presentation.base.BaseViewModel;
 import com.add.ad.presentation.base.SingleLiveEvent;
+import com.add.ad.presentation.util.TagList;
 
 import java.util.ArrayList;
 
@@ -28,7 +29,11 @@ public class SearchViewModel extends BaseViewModel {
 
     public MutableLiveData<String> searchEt = new MutableLiveData<>(" ");
     public MutableLiveData<ArrayList<ResponseSearchInfo>> searchList = new MutableLiveData<>();
+    public MutableLiveData<ResponseSearchInfo> detailSearch = new MutableLiveData<>();
+
+
     public SingleLiveEvent<Void> searchListEvent = new SingleLiveEvent<>();
+    public SingleLiveEvent<Void> searchDetailEvent = new SingleLiveEvent<>();
 
     public void getCreatorList() {
         compositeDisposable.add(searchRepository.getCreators(0)
@@ -48,12 +53,44 @@ public class SearchViewModel extends BaseViewModel {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io())
                 .subscribe(it -> {
-                    if(it.code() == 200){
+                    if (it.code() == 200) {
                         searchList.setValue(it.body());
                         searchListEvent.call();
                     }
                 }, it -> {
                     Log.e("error", it.getMessage());
+                }));
+    }
+
+    public void searchTag(int position) {
+        Log.d("position", TagList.getTagList().get(position));
+        compositeDisposable.add(searchRepository.searchTag(0, TagList.getTagList().get(position))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(it -> {
+                    switch (it.code()) {
+                        case 200: {
+                            searchList.setValue(it.body());
+                            searchListEvent.call();
+                        }
+                    }
+                }, it -> {
+                    createToastEvent.setValue(it.getLocalizedMessage());
+                }));
+    }
+
+    public void searchDetailCreator(int position) {
+        compositeDisposable.add(searchRepository.getDetailCreator(searchList.getValue().get(position).getUserId())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(it -> {
+                    Log.d("detailCode", String.valueOf(it.code()));
+                    if(it.code() == 200){
+                        detailSearch.setValue(it.body());
+                        Log.d("detailCodeasdfasdfasdf", detailSearch.getValue().getUserEmail() + "dfadfa");
+                    }
+                }, it -> {
+                    Log.e("detailError", it.getMessage());
                 }));
     }
 }
