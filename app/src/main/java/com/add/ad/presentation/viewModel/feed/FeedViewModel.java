d803d352bc510e78e9047bff9c7979d09cdeaf55
윤석준 <tjrwns8024@naver.com>
2020-11-14 23:31:32 +0900

    [ADD[ <viewmodel/ FeedViewModel.java> add clickBack func

--- app/src/main/java/com/add/ad/presentation/viewModel/feed/FeedViewModel.java
+++ app/src/main/java/com/add/ad/presentation/viewModel/feed/FeedViewModel.java
@@ -1,7 +1,5 @@
 package com.add.ad.presentation.viewModel.feed;
 
-import android.util.Log;
-
 import androidx.hilt.Assisted;
 import androidx.hilt.lifecycle.ViewModelInject;
 import androidx.lifecycle.MutableLiveData;
@@ -13,7 +11,6 @@ import com.add.ad.presentation.base.BaseViewModel;
 import com.add.ad.presentation.base.SingleLiveEvent;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
@@ -46,23 +43,21 @@ public class FeedViewModel extends BaseViewModel {
                             feedListEvent.call();
                         }
                     }
-                }, it -> {
-                    Log.e("errorFeed", it.getMessage());
-                }));
+                }, it -> createToastEvent.setValue("알 수 없는 오류가 발생하였습니다.")));
     }
 
     public void getDetailFeed(int position) {
-        Log.d("post", String.valueOf(feedList.getValue().get(position).getFeedId()));
-
         compositeDisposable.add(feedRepository.getDetailFeed(feedList.getValue().get(position).getFeedId())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io())
                 .subscribe(it -> {
-                    if(it.code() == 200){
+                    if (it.code() == 200) {
                         detailFeed.setValue(it.body());
                     }
-                }, it -> {
-                    Log.e("errer", it.getMessage());
-                }));
+                }, it -> createToastEvent.setValue("알 수 없는 오류가 발생하였습니다.")));
+    }
+
+    public void clickBack(){
+        backEvent.call();
     }
 }
