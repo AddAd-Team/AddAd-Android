758d592ed190aed6e2d89bf4538629f5bb5c3a19
윤석준 <tjrwns8024@naver.com>
2020-11-16 13:56:16 +0900

    [ADD] <repository/ FeedRepository *.java> add post, delete Like, post Apply

--- app/src/main/java/com/add/ad/data/repository/feed/FeedRepository.java
+++ app/src/main/java/com/add/ad/data/repository/feed/FeedRepository.java
@@ -8,9 +8,18 @@ import java.util.List;
 
 import io.reactivex.Single;
 import retrofit2.Response;
+import retrofit2.http.DELETE;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface FeedRepository {
     Single<Response<ArrayList<ResponseFeedInfo>>> getFeed();
 
     Single<Response<ResponseFeedInfo>> getDetailFeed(int postId);
+
+    Single<Response<Void>> postLike(int postId);
+
+    Single<Response<Void>> deleteLike(int postId);
+
+    Single<Response<Void>> postApply(int postId);
 }
