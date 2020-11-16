19de3071df1aa4ad4ed6c354e4ecb0a97278b727
윤석준 <tjrwns8024@naver.com>
2020-11-16 19:17:11 +0900

    [CREATE] <fragment/ AdLikeFragment.java> create interested Ad Fragment

---
+++ app/src/main/java/com/add/ad/presentation/ui/fragment/main/mypage/etc/like/AdLikeFragment.java
@@ -0,0 +1,18 @@
+package com.add.ad.presentation.ui.fragment.main.mypage.etc.like;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.add.ad.R;
+
+public class AdLikeFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ad_like, container, false);
+    }
+}
