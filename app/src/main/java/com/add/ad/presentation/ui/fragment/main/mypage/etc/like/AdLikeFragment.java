f405689ee2cf868f4431426145d8a892a65b08fe
윤석준 <tjrwns8024@naver.com>
2020-11-18 11:50:44 +0900

    [ADD] <fragment/ AdLikeFragment.java> add observeEvent, VmOwner, ViewModelClass

--- app/src/main/java/com/add/ad/presentation/ui/fragment/main/mypage/etc/like/AdLikeFragment.java
+++ app/src/main/java/com/add/ad/presentation/ui/fragment/main/mypage/etc/like/AdLikeFragment.java
@@ -3,16 +3,52 @@ package com.add.ad.presentation.ui.fragment.main.mypage.etc.like;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.add.ad.R;
+import com.add.ad.databinding.FragmentAdLikeBinding;
+import com.add.ad.presentation.adapter.LikeAdAdapter;
+import com.add.ad.presentation.adapter.SearchAdapter;
+import com.add.ad.presentation.base.BaseFragment;
+import com.add.ad.presentation.viewModel.mypage.myad.LikeAdViewModel;
 
-public class AdLikeFragment extends Fragment {
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class AdLikeFragment extends BaseFragment<FragmentAdLikeBinding, LikeAdViewModel> {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ad_like, container, false);
+        setLayout(R.layout.fragment_ad_like);
+        View v = super.onCreateView(inflater, container, savedInstanceState);
+
+        viewModel.getLikeAdList();
+
+        return v;
+    }
+
+
+
+    @Override
+    protected Class<LikeAdViewModel> getViewModelClass() {
+        return LikeAdViewModel.class;
+    }
+
+    @Override
+    protected ViewModelStoreOwner getVmOwner() {
+        return requireActivity();
+    }
+
+    @Override
+    protected void observeEvent() {
+        viewModel.likeAdEvent.observe(this, mVoid -> {
+            binding.adLikeRecyclerView.setAdapter(new LikeAdAdapter(viewModel.likeAdList.getValue(), viewModel));
+            binding.adLikeRecyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        });
     }
 }
