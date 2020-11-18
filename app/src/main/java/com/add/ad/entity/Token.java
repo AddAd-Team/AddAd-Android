e9fbc4c6325aae598ef9fb946053b78b4e50bcad
윤석준 <tjrwns8024@naver.com>
2020-11-18 11:46:06 +0900

    [DELETE, ADD] <entity/ Token.java> add userInfo field, delete setter

--- app/src/main/java/com/add/ad/entity/Token.java
+++ app/src/main/java/com/add/ad/entity/Token.java
@@ -3,25 +3,17 @@ package com.add.ad.entity;
 public class Token {
     private String accessToken;
     private String refreshToken;
-
-    public Token(String accessToken, String refreshToken){
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-    }
+    private String userInfo;
 
     public String getAccessToken() {
         return accessToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
     }
 
     public String getRefreshToken() {
         return refreshToken;
     }
 
-    public void setRefreshToken(String refreshToken) {
-        this.refreshToken = refreshToken;
+    public String getUserInfo() {
+        return userInfo;
     }
 }
