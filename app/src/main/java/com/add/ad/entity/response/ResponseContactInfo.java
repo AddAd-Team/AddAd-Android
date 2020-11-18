96717945ff54f0bb1d1e266d8f8c1daf372f216a
윤석준 <tjrwns8024@naver.com>
2020-11-18 15:50:40 +0900

    [CREATE] <entity/ ResponseContactInfo.java> create contact response entity

---
+++ app/src/main/java/com/add/ad/entity/response/ResponseContactInfo.java
@@ -0,0 +1,39 @@
+package com.add.ad.entity.response;
+
+public class ResponseContactInfo {
+    private String title;
+    private int creatorId;
+    private String creatorProfileImage;
+    private String creatorName;
+    private int advertiserId;
+    private String advertiserProfileImage;
+    private String advertiserName;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getCreatorId() {
+        return creatorId;
+    }
+
+    public String getCreatorProfileImage() {
+        return creatorProfileImage;
+    }
+
+    public String getCreatorName() {
+        return creatorName;
+    }
+
+    public int getAdvertiserId() {
+        return advertiserId;
+    }
+
+    public String getAdvertiserProfileImage() {
+        return advertiserProfileImage;
+    }
+
+    public String getAdvertiserName() {
+        return advertiserName;
+    }
+}
