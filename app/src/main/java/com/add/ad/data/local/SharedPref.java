42f4925e66067cf8f558c5d2c8b83e73523e168e
윤석준 <tjrwns8024@naver.com>
2020-11-18 11:43:58 +0900

    [ADD] <local/ SharedPref.java> add saveInfo, getInfo funcion -> To get User Info(creator, advertiser)

--- app/src/main/java/com/add/ad/data/local/SharedPref.java
+++ app/src/main/java/com/add/ad/data/local/SharedPref.java
@@ -14,24 +14,33 @@ public class SharedPref {
     }
 
     public void saveToken(String token, Boolean access){
-        getPref(context).edit().putString(getKey(access),token).apply();
+        getPref(context, true).edit().putString(getKey(access),token).apply();
     }
 
     public String getToken(Boolean access){
-        if(getPref(context).getString(getKey(access), "").isEmpty()) {
+        if(getPref(context,true).getString(getKey(access), "").isEmpty()) {
             return "";
-        } else return "Bearer " + getPref(context).getString(getKey(access), "");
+        } else return "Bearer " + getPref(context,true).getString(getKey(access), "");
+    }
+
+    public void saveInfo(String info, Boolean bool){
+        getPref(context, false).edit().putString(getKey(bool),info).apply();
+    }
+
+    public String getInfo(Boolean bool){
+         return getPref(context,false).getString(getKey(bool), "");
     }
 
     public void removeToken(Boolean bool){
-        getPref(context).edit().remove(getKey(bool)).apply();
+        getPref(context, true).edit().remove(getKey(bool)).apply();
     }
-    private SharedPreferences getPref(Context context) {
-        return context.getSharedPreferences("pref", Context.MODE_PRIVATE);
+
+    private SharedPreferences getPref(Context context, Boolean bool) {
+        return bool ? context.getSharedPreferences("auth", Context.MODE_PRIVATE) : context.getSharedPreferences("info", Context.MODE_PRIVATE);
     }
 
     private String getKey(Boolean access) {
         if (access) return "Access";
-        else return "Refresh";
+        else return "Info";
     }
 }
