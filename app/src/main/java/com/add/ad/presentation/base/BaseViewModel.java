294d894bb35c972d06e315071ce059a907fd1655
윤석준 <tjrwns8024@naver.com>
2020-11-27 11:49:11 +0900

    [ADD] <base/ Base*.java> add snackbar event

--- app/src/main/java/com/add/ad/presentation/base/BaseViewModel.java
+++ app/src/main/java/com/add/ad/presentation/base/BaseViewModel.java
@@ -3,6 +3,7 @@ package com.add.ad.presentation.base;
 import androidx.lifecycle.ViewModel;
 
 public abstract class BaseViewModel extends ViewModel {
+    public SingleLiveEvent<String> createSnackEvent = new SingleLiveEvent<>();
     public SingleLiveEvent<String> createToastEvent = new SingleLiveEvent<>();
     public SingleLiveEvent<Void> createProgressEvent = new SingleLiveEvent<>();
     public SingleLiveEvent<Void> dismissProgressEvent = new SingleLiveEvent<>();
